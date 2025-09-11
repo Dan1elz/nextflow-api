e2fda87994ff55e8d2120d0523ffdc411cd4a21c
matsushitakoiti <matsushitakoiti@gmail.com>
2025-09-10 22:49:22 -0300

    Task: Criação dos Model Country, State e City

---
+++ nextflow.Domain/Models/Country.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using nextflow.Domain.Models.Base;
+using Nextflow.Domain.Dtos;
+
+namespace Nextflow.Domain.Models;
+
+[Table("countries")]
+public class Country : BaseModel
+{
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O Nome do país deve ter no máximo 100 caracteres e no mínimo 2 caracteres."), Required(ErrorMessage = "O Nome do país é obrigatório.")]
+    public string Name { get; private set; } = string.Empty;
+
+    [StringLength(2, MinimumLength = 2, ErrorMessage = "O acrônimo do país deve ter no máximo 2 caracteres e no mínimo 2 caracteres."), Required(ErrorMessage = "O acrônimo do país é obrigatório.")]
+    public string AcronymIso { get; private set; } = string.Empty;
+
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "O código Sefaz deve ter no máximo 255 caracteres e no mínimo 2 caracteres."), Required(ErrorMessage = "O código Sefaz é obrigatório.")]
+    public string SefazCode { get; private set; } = string.Empty;
+
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "O código do Bacen deve ter no máximo 255 caracteres e no mínimo 2 caracteres.")]
+    public string BacenCode { get; private set; } = string.Empty;
+    public virtual ICollection<State> States { get; set; } = [];
+
+    private Country() : base() { }
+
+    public Country(CreateCountryDto dto) : base()
+    {
+        Name = dto.Name;
+        AcronymIso = dto.AcronymIso;
+        SefazCode = dto.SefazCode;
+        BacenCode = dto.BacenCode;
+    }
+
+    public void Update(UpdateCountryDto dto)
+    {
+        Name = dto.Name;
+        AcronymIso = dto.AcronymIso;
+        SefazCode = dto.SefazCode;
+        BacenCode = dto.BacenCode;
+        base.Update();
+    }
+
+}
