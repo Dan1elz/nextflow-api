eda6c134650cc33c55f5ac23dbb4155513a45901
matsushitakoiti <matsushitakoiti@gmail.com>
2025-10-22 20:11:36 -0300

    Finalização dos controllers de Suppliers e States

---
+++ nextflow/Controllers/StatesController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using nextflow.Attributes;
+using nextflow.Domain.Dtos;
+using nextflow.Domain.Enums;
+using nextflow.Domain.Interfaces.UseCases.Base;
+using nextflow.Domain.Models;
+using nextflow.Dtos;
+using nextflow.Utils;
+using Nextflow.Domain.Dtos;
+using Nextflow.Domain.Models;
+
+
+namespace Nextflow.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class StatesController(
+    ICreateUseCase<CreateCountryDto, CountryResponseDto> createUseCase,
+    IUpdateUseCase<UpdateCountryDto, CountryResponseDto> updateUseCase,
+    IDeleteUseCase deleteUseCase,
+    IGetAllUseCase<Country, CountryResponseDto> getAllCountrysUseCase,
+    IGetByIdUseCase<CountryResponseDto> getCountryByIdUseCase
+) : ControllerBase
+{
+    [HttpPost]
+    [RoleAuthorize(RoleEnum.Admin)]
+    public async Task<IActionResult> Create([FromBody] CreateCountryDto dto, CancellationToken ct)
+    {
+        var entity = await createUseCase.Execute(dto, ct);
+
+        return CreatedAtAction(nameof(GetById), new { id = entity.Id }, entity);
+    }
+
+    [HttpPut("{id:guid}")]
+    [RoleAuthorize(RoleEnum.Admin)]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateCountryDto dto, CancellationToken ct)
+    {
+        return Ok(new ApiResponse<CountryResponseDto>
+        {
+            Status = 200,
+            Message = "Estado atualizado com sucesso.",
+            Data = await updateUseCase.Execute(id, dto, ct)
+        });
+    }
+
+    [HttpDelete("{id:guid}")]
+    [RoleAuthorize(RoleEnum.Admin)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken ct)
+    {
+        await deleteUseCase.Execute(id, ct);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] int offset = 0, [FromQuery] int limit = 10, CancellationToken ct = default)
+    {
+        return Ok(new ApiResponse<ApiResponseTable<CountryResponseDto>>
+        {
+            Status = 200,
+            Message = "Estados recuperados com sucesso.",
+            Data = await getAllCountrysUseCase.Execute(u => u.IsActive == true, offset, limit, ct)
+        });
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken ct)
+    {
+        return Ok(new ApiResponse<CountryResponseDto>
+        {
+            Status = 200,
+            Message = "Estado recuperado com sucesso.",
+            Data = await getCountryByIdUseCase.Execute(id, ct)
+        });
+    }
+}
---
+++ nextflow/Controllers/SuppliersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using nextflow.Attributes;
+using nextflow.Domain.Dtos;
+using nextflow.Domain.Enums;
+using nextflow.Domain.Interfaces.UseCases.Base;
+using Nextflow.Domain.Dtos;
+using Nextflow.Domain.Models;
+
+namespace Nextflow.Controllers;
+
+public class SuppliersController(
+    ICreateUseCase<CreateSupplierDto, SupplierResponseDto> createUseCase,
+    IUpdateUseCase<UpdateSupplierDto, SupplierResponseDto> updateUseCase,
+    IDeleteUseCase deleteUseCase,
+    IGetAllUseCase<Supplier, SupplierResponseDto> getAllSuppliersUseCase,
+    IGetByIdUseCase<SupplierResponseDto> getSupplierByIdUseCase
+) : ControllerBase
+{
+    [HttpPost]
+    [RoleAuthorize(RoleEnum.Admin)]
+    public async Task<IActionResult> Create([FromBody] CreateSupplierDto dto, CancellationToken ct)
+    {
+        var createdSupplier = await createUseCase.Execute(dto, ct);
+        return CreatedAtAction(nameof(GetById), new { id = createdSupplier.Id }, createdSupplier);
+    }
+
+    [HttpPut("{id:guid}")]
+    [RoleAuthorize(RoleEnum.Admin)]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateSupplierDto dto, CancellationToken ct)
+    {
+        return Ok(new ApiResponse<SupplierResponseDto>
+        {
+            Status = 200,
+            Message = "Fornecedor atualizado com sucesso.",
+            Data = await updateUseCase.Execute(id, dto, ct)
+        });
+    }
+
+    [HttpDelete("{id:guid}")]
+    [RoleAuthorize(RoleEnum.Admin)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken ct)
+    {
+        await deleteUseCase.Execute(id, ct);
+
+        return NoContent();
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] int offset = 0, [FromQuery] int limit = 10, CancellationToken ct = default)
+    {
+        return Ok(new ApiResponse<ApiResponseTable<SupplierResponseDto>>
+        {
+            Status = 200,
+            Message = "Fornecedor recuperados com sucesso.",
+            Data = await getAllSuppliersUseCase.Execute(u => u.IsActive == true, offset, limit, ct)
+        });
+    }
+
+    [Authorize]
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken ct)
+    {
+        return Ok(new ApiResponse<SupplierResponseDto>
+        {
+            Status = 200,
+            Message = "Fornecedor recuperado com sucesso.",
+            Data = await getSupplierByIdUseCase.Execute(id, ct)
+        });
+    }
+}
+
+
